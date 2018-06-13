445a3e79815bed8f190235419abb5f3fa85028a2
svetoslavmishev <svetlioo.m@gmail.com>
2018-06-13 06:58:25 +0300

    Category bug fix

--- Shop-Stop/handlers/product-handler.js
+++ Shop-Stop/handlers/product-handler.js
@@ -4,6 +4,7 @@ const multiparty = require('multiparty');
 const path = './views/products/add.html';
 const querystring = require('querystring');
 const Product = require('../model/Product');
+const Category = require('../model/Category');
 
 module.exports = (req, res) => {
     if (req.method === 'GET' && req.pathname === '/product/add') {
@@ -11,12 +12,25 @@ module.exports = (req, res) => {
             if (err) {
                 console.log(err);
             }
-
-            res.writeHead(200, {
-                'content-type': 'text/html'
-            });
-            res.write(data);
-            res.end();
+            
+            Category
+                .find()
+                .then(categories => {
+                    let replacer = '<select class="input-field" name="category">';
+                    
+                    for(let category of categories) {
+                    	replacer += `<option value="${category._id}">${category.name}</option>`;                    	
+                    }
+                    replacer += '</select>';                    
+                    let html = data.toString().replace('{categories}', replacer);
+                    
+                    res.writeHead(200, {
+                        'content-type': 'text/html'
+                    });
+                    
+		    res.write(html);
+                    res.end();
+                });
         });
     } else if (req.method === 'POST' && req.pathname === '/product/add') {
         let form = new multiparty.Form();
@@ -58,16 +72,24 @@ module.exports = (req, res) => {
         });
 
         form.on('close', () => {
-            //database.products.add(product);
 
             Product
                 .create(product)
-                .then(() => {
-                    res.writeHead(302, {
-                        Location: '/'
-                    });
-
-                    res.end();
+                .then(newProduct => {
+                     Category
+                     .findById(product.category)
+                     .then(category => {
+                     	category.products.push(newProduct._id);
+                     	category
+                     	.save()
+                     	.then(() => {
+                     	    res.writeHead(302, {
+                     	    	location: '/'
+                     	    });	
+                     	    
+                     	    res.end();
+                     	})
+                     });
                 });
         });
 
